ac076387f9474de6f16e30dec4dc77d1af3ae872
cbrutscher <cbrutscher@liveperson.com>
2022-04-19 14:18:58 +0200

    🔥 removed biggestboi

--- supertest/functions/biggestboi/index.js
+++
@@ -1,5 +0,0 @@
-function lambda(input, callback) {
-  console.info('is this real life?');
-  console.error('it is just phantasy!');
-  callback(null, `Hello World`);
-}
--- supertest/functions/loggingTemplate/index.js
+++
@@ -1,7 +0,0 @@
-function lambda(input, callback) {
-    console.debug("Debugging log");
-    console.warn("Warn log");
-    setTimeout(() => {
-        callback(null, `Hello World`);
-    }, 31 * 1000)
-}
