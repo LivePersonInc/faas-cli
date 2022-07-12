fb61936f80aba5a516ec08eedc2215a3648feebe
cbrutscher <cbrutscher@liveperson.com>
2022-07-12 13:57:16 +0200

    🐛 increased e2e test timeout

--- e2e/deployAndInvoke.e2e.test.ts
+++ e2e/deployAndInvoke.e2e.test.ts
@@ -7,6 +7,8 @@ const password = process.env.E2E_TEST_PASSWORD;
 const exec = promisify(require('child_process').exec);
 
 describe('Full flow e2e test', () => {
+  const cwd = process.cwd();
+
   test('Is able to make terminal input and view in-progress stdout', async () => {
     const filesToDelete = [
       'functions',
@@ -20,25 +22,28 @@ describe('Full flow e2e test', () => {
     await exec(`rm -rf ${filesToDelete}`);
 
     const loginCmd = await exec(
-      `node ../bin/dev login -a ${accountId} -u ${username} -p ${password}`,
+      `node ${cwd}/bin/dev login -a ${accountId} -u ${username} -p ${password}`,
     );
+
     expect(loginCmd.stdout).toContain('Welcome to');
 
     const fnName = 'exampleFunction';
 
-    const initCmd = await exec(`node ../bin/dev init`);
+    const initCmd = await exec(`node ${cwd}/bin/dev init`);
     expect(initCmd.stdout).toContain('Initializing structure [completed]');
 
-    const pullCmd = await exec(`node ../bin/dev pull ${fnName} --yes`);
+    const pullCmd = await exec(`node ${cwd}/bin/dev pull ${fnName} --yes`);
     expect(pullCmd.stdout).toContain('Pulling exampleFunction [completed]');
 
-    const deployCmd = await exec(`node ../bin/dev deploy ${fnName} --yes`);
+    const deployCmd = await exec(`node ${cwd}/bin/dev deploy ${fnName} --yes`);
     expect(deployCmd.stdout).toContain('Deploying exampleFunction [completed]');
 
-    const invokeCmd = await exec(`node ../bin/dev invoke ${fnName}`);
+    const invokeCmd = await exec(`node ${cwd}/bin/dev invoke ${fnName}`);
     expect(invokeCmd.stdout).toContain('Hello World');
 
-    const undeployCmd = await exec(`node ../bin/dev undeploy  ${fnName} --yes`);
+    const undeployCmd = await exec(
+      `node ${cwd}/bin/dev undeploy  ${fnName} --yes`,
+    );
     expect(undeployCmd.stdout).toContain(
       'Undeploying exampleFunction [completed]',
     );
